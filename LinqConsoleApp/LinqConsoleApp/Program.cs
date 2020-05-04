8bb94b4eae8b8dacf98edfc5bb4889437daacf72
s18371 <s18371@pjwstk.edu.pl>
2020-05-04 19:22:01 +0200

    Zrobione ćw 9 linq

---
+++ LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            d.Przyklad4();
+
+            
+        }
+    }
+}
